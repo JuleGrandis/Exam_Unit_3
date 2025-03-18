2537aa7f8c07dfd182c231fb98297effea811f19
JuleGrandis <AleksanderNordrumBuseth@gmail.com>
2025-03-18 18:16:12 +0100

    created first iteration of submiteAnswer function. Following simiular logic and setup as prev function.

--- alchemy.mjs
+++ alchemy.mjs
@@ -4,6 +4,7 @@ const API_URL = 'https://alchemy-kd0l.onrender.com';
 const PLAYER_NAME = 'aleksandnd@uia.no';
 
 async function startGame(playerName) {
+
     try {
         const response = await fetch(`${API_URL}/start?player=${playerName}`);
         const data = await response.json();
@@ -13,7 +14,33 @@ async function startGame(playerName) {
         } else {
             console.log(`Challenge: ${data.challenge}`);
         }
+
     } catch (error) {
         console.error('Failed to start the game.', error);
+    }
+}
+
+async function submitAnswer (playerName, answer) {
+
+    try {
+        const response = await fetch(`${API_URL}/answer`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringify({ player: playerName, answer: answer}),
+        });
+
+        const data = await response.json();
+
+        if(data.error) {
+            console.error(`Error: ${data.error}`);
+        } else {
+            console.log(`Message: ${data.message}`);
+            if (data.next_challenge) {
+                console.log(`Next Challenge: ${data.next_challenge}`);
+            }
+        }
+        
+    } catch (error) {
+        console.error('Failed to submit answer:', error);
     }
 }
